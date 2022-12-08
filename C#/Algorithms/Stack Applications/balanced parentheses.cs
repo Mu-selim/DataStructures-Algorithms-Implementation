429e2b0288f73791e085673d92a2d97e56ed7c51
selim <muhamadselim70@gmail.com>
2022-12-08 18:17:05 +0200

    Stack Applications

---
+++ C#/Algorithms/Stack Applications/balanced parentheses.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+namespace Stack_Applications
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("{}: "+ isBalanced("{}"));
+            Console.WriteLine("{()}: " + isBalanced("{()}"));
+            Console.WriteLine("{)}: "+ isBalanced("{)}"));
+            Console.WriteLine("{(}): " + isBalanced("{(})"));
+            Console.WriteLine("{(())()[()]}: "+ isBalanced("{(())()[()]}"));
+            Console.WriteLine("{(())()[([{}])]}: "+ isBalanced("{(())()[([{}])]}"));
+            Console.WriteLine("((12+5)*4): "+ isBalanced("((12+5)*4)"));
+            Console.WriteLine("((12+5)*(4)): "+ isBalanced("((12+5)*(4))"));
+            Console.WriteLine("((12+5)*(4): "+ isBalanced("((12+5)*(4)"));
+            Console.WriteLine("((12+5)*(4)/12): "+ isBalanced("((12+5)*(4)/12)"));
+        }
+
+        public static bool arePair(char c1, char c2)
+        {
+            if ((c1 == '(' && c2 == ')') || (c1 == '[' && c2 == ']') || (c1 == '{' && c2 == '}'))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool isBalanced(string s)
+        {
+            Stack stack = new Stack();
+            int length = s.Length;
+            for (int i = 0; i < length; i++)
+            {
+                char temp = s[i];
+                if (temp == '(' || temp == '[' || temp == '{')
+                {
+                    stack.Push(temp);
+                    continue;
+                }
+
+                if (temp == ')' || temp == ']' || temp == '}')
+                {
+                    if (stack.Count == 0 || !arePair(Convert.ToChar(stack.Pop()), temp))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return stack.Count == 0;
+        }
+    }
+}
