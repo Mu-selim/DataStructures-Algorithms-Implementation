7d173144dd22ff1c22324ea418133fbb497bf2c3
selim <muhamadselim70@gmail.com>
2022-12-18 11:42:26 +0200

    739. Daily Temperatures

--- Leetcode problems/739. Daily Temperatures.js
+++ Leetcode problems/739. Daily Temperatures.js
@@ -3,5 +3,18 @@
  * @return {number[]}
  */
 var dailyTemperatures = function(temperatures) {
-    
+    let len = temperatures.length;
+    let res = new Array(len).fill(0), stack = [];
+    for(let i = 0; i < len; i++) {
+        while(stack.length > 0 && temperatures[i] > temperatures[stack[stack.length-1]]) {
+            let index = stack.pop();
+            res[index] = i - index;
+        }
+        stack.push(i);
+    }
+    return res;
 };
+
+console.log(dailyTemperatures(temperatures = [73,74,75,71,69,72,76,73]));
+console.log(dailyTemperatures(temperatures = [30,40,50,60]));
+console.log(dailyTemperatures(temperatures = [30,60,90]));
